120df189382a0264eceb3cc5f02d040a976c7962
Julien <julien.juret@gmail.com>
2020-08-05 11:58:57 +0200

    initial state

---
+++ src/components/Recipes/RecipesListScreen/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default RecipesListScreen = ({}) => {
+    return <View>
+        <Text>RecipesListScreen</Text>
+    </View>
+}
