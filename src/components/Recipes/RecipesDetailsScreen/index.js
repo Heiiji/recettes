29d677b1badfca0d98e5c5b3a8d742c95ed6a87e
Julien <julien.juret@gmail.com>
2020-08-07 10:03:14 +0200

    add more details on details screen

--- src/components/Recipes/RecipesDetailsScreen/index.js
+++ src/components/Recipes/RecipesDetailsScreen/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 import {fetchSelectedRecipe} from "../../../api/recipes";
 import {useDispatch, useSelector} from "react-redux";
 import { getSelectedRecipe } from "../../../redux/selectors";
@@ -13,12 +13,46 @@ const RecipesDetailsScreen = ({navigation, route}) => {
         fetchSelectedRecipe(dispatch, id);
     }, []);
 
-    return <View>
-        <Text>RecipesDetailsScreen</Text>
-        <Text>{recipe.title}</Text>
-        <Button title="Revenir" onPress={() => {
-            navigation.goBack();
-        }}/>
-    </View>
+    return <ScrollView>
+        <View style={styles.containerImage}>
+            <Image source={{ uri: recipe.image }} resizeMode={"cover"} style={styles.image} />
+        </View>
+        <Text style={styles.title}>
+            {recipe.title}
+        </Text>
+        <View style={styles.containerIngredients}>
+            {
+                recipe.extendedIngredients.map(ing => {
+                    return <Text key={ing.name} style={styles.ing} >{ing.name}</Text>
+                })
+            }
+        </View>
+    </ScrollView>
 }
+
+const styles = StyleSheet.create({
+    containerImage: {
+        height: 200
+    },
+    image: {
+        height: 200
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 26,
+        textAlign: "center",
+        marginVertical: 10
+    },
+    containerIngredients: {
+        borderTopWidth:  1,
+        borderTopColor: "grey",
+        borderBottomWidth: 1,
+        borderBottomColor: "grey",
+        padding: 15
+    },
+    ing :{
+        fontSize: 16
+    }
+});
+
 export default RecipesDetailsScreen;
