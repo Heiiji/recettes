ddabfc022978d36380ae936d07c8f89cdcc19e00
Julien <julien.juret@gmail.com>
2020-08-10 14:19:38 +0200

    [CHANGE] - manual search

--- src/components/_Shared/Search/search.js
+++ src/components/_Shared/Search/search.js
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
-import {View, StyleSheet, TextInput} from "react-native";
-import {useDispatch} from "react-redux";
+import {View, StyleSheet, TextInput, Button} from "react-native";
+import {useDispatch, useSelector} from "react-redux";
 import { setSearchText } from "../../../redux/actions";
+import {getSearch} from "../../../redux/selectors";
 
 const Search = () => {
     const [text, setText] = useState("");
+    const search = useSelector(getSearch);
     const dispatch = useDispatch();
 
     const _onChangeText = (value) => {
         setText(value);
-        dispatch(setSearchText(value));
+    }
+
+    const _onSearch = () => {
+        dispatch(setSearchText(text));
     }
 
     return (
         <View style={styles.container}>
             <TextInput style={styles.input} value={text} placeholder="Search" onChangeText={_onChangeText} />
+            {
+                search.query !== text && <Button title="Search" onPress={_onSearch} />
+            }
         </View>
     );
 }
@@ -22,6 +30,7 @@ const Search = () => {
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
+        alignItems: "center"
     },
     input: {
         padding: 5,
